bd0fa729cb67b2dd91374688b657f626afa79b0a
Luis Felipe de Melo <felipe_melo_1@hotmail.com>
2015-07-16 15:42:44 -0300

    Adição de fontes e css na pasta de fontes para estilização delas.

--- header.php
+++ header.php
@@ -1,12 +1,15 @@
-<?php
-?>
+<?php ?>
+<link rel="stylesheet" type="text/css" href="/fonts/fonts.css" />
 <link rel="stylesheet" href="css/header-footer.css" />
 <body>
-<div id="fixedheader">
-	<img border="0" src="http://logo.empregos.com.br/142914_G.jpg" />
-	<a id="inicio" href="index.php"> Inicio</a>
-</div>
+	<div id="fixedheader">
+		<img border="0" src="http://logo.empregos.com.br/142914_G.jpg" />
+		<a id="inicio" href="index.php"> Inicio</a>
+	</div>
 
-
-<div id="fixedfooter">Bottom div content</div>
+	<div id="fixedfooter">
+		<p>
+			Núcleo de Desenvolvimento Institucional
+		</p>
+	</div>
 </body>
--- hr_panel.php
+++ hr_panel.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 include "header.php";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Painel de avisos RH</title>
-    </head>
-    <body>
-        
-        </body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Painel de avisos RH</title>
+	</head>
+	<body>
+
+	</body>
 </html>
