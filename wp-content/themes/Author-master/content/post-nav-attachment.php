ac423cd3524c8fef0c58ebceed68a7cf7faf1bdf
Luis Felipe de Melo <felipe_melo_1@hotmail.com>
2015-07-28 17:37:20 -0300

    Deletados alguns temas não utilizados.

--- wp-content/themes/Author-master/content/post-nav-attachment.php
+++
@@ -1,8 +0,0 @@
-<nav class="further-reading">
-	<div class="previous">
-		<p><?php previous_image_link( false, __( 'Previous Image', 'author' ) ); ?></[>
-	</div>
-	<div class="next">
-		<p><?php next_image_link(false, __( 'Next Image', 'author' )); ?></p>
-	</div>
-</nav>
