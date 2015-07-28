661bed3addf7e7c77a3e4a3c035d89f3520c3916
Luis Felipe de Melo <felipe_melo_1@hotmail.com>
2015-07-28 17:36:32 -0300

    Segunda versão com interface pronta, sem alimentação de conteúdo.

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,6 @@ define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
